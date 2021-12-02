3a23048c6099d6a9f56ded60289f2b2986cd416f
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-12-02 20:00:31 +0530

    Welcome to address book program

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        System.out.println("**************WELCOME TO ADDRESS BOOK**************");
+    }
+}
